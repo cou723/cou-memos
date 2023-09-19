ffceb83060ab22cbcf9cccd1fd6fb34e7206aebe
cou723 <66350686+cou723@users.noreply.github.com>
2023-09-19 07:02:41 +0000

    smartphone test

--- src/hooks/useConfigFile.ts
+++ src/hooks/useConfigFile.ts
@@ -1,17 +1,27 @@
 import { api } from "@/lib/api";
 import { Config } from "@/types/config";
-import { useEffect, useState } from "react";
+import { useEffect, useState, useContext } from "react";
+import { NotificationStack } from "@/provider/NotificationProvider";
 
-export function useConfigFile(): [Config, React.Dispatch<React.SetStateAction<Config>>] {
-    const [config, setConfig] = useState<Config>({ data_path: "" } as Config);
+export function useConfigFile(): [
+  Config,
+  React.Dispatch<React.SetStateAction<Config>>
+] {
+  const [config, setConfig] = useState<Config>({ data_path: "" } as Config);
+  const { notifications, dispatch } = useContext(NotificationStack);
 
-    useEffect(() => {
-        const fetchConfig = async () => {
-            let config = (await api.get_config()).unwrap();
-            setConfig(config);
-        };
-        fetchConfig();
-    }, []);
+  useEffect(() => {
+    const fetchConfig = async () => {
+      let config = await api.get_config();
+      if (config.err)
+        dispatch({
+          type: "push",
+          value: { type: "error", message: "configの取得に失敗しました" },
+        });
+      else setConfig(config);
+    };
+    fetchConfig();
+  }, []);
 
-    return [config, setConfig];
+  return [config, setConfig];
 }
