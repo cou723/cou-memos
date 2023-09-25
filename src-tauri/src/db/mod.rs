af0b7f9d0451c96ee3c17d08d364a073b0dbcf76
cou723 <courange.c@gmail.com>
2023-09-25 15:36:08 +0900

    add log more

--- src-tauri/src/db/mod.rs
+++ src-tauri/src/db/mod.rs
@@ -10,14 +10,19 @@ pub mod tag;
 no_arg_sql_function!(last_insert_rowid, Integer);
 
 pub fn establish_connection() -> Result<SqliteConnection, Error> {
+    println!("get config");
     let config = config::get().map_err(|_| Error::ConfigNotFound)?;
+    println!("got config");
 
     let path = Path::new(&config.data_path);
 
+    println!("path: {:?}", path);
     if !path.exists() {
         fs::create_dir_all(path).map_err(|_| Error::CreateDirectoryFailed)?;
     }
+    println!("path exists");
 
+    println!("get connection");
     let db_path = config.data_path + "/c_memos";
     let conn = match SqliteConnection::establish(&db_path) {
         Ok(v) => v,
@@ -30,14 +35,18 @@ pub fn establish_connection() -> Result<SqliteConnection, Error> {
                 .map_err(|_| Error::DbNotFound)?
         }
     };
+    println!("got connection");
 
+    println!("any_pending_migrations");
     match any_pending_migrations(&conn) {
         Ok(is) => {
             if is {
+                println!("run migration");
                 run_pending_migrations(&conn).map_err(|_| Error::DbMigrationFailed)?;
             }
         }
         Err(_) => {
+            println!("run migration");
             run_pending_migrations(&conn).map_err(|_| Error::DbMigrationFailed)?;
         }
     }
