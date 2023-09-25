eb4af66660bc05d025f2315cd864a595857761e1
cou723 <courange.c@gmail.com>
2023-09-25 17:26:27 +0900

    add migrations

--- src/hooks/useToast.ts
+++
@@ -1,7 +0,0 @@
-import { useState } from "react";
-
-export function useToast() {
-    const [notificationStack, setNotificationStack] = useState<string[]>([]);
-
-    return { pushNotification, notificationStack, popNotification };
-}
