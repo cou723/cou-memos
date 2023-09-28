676040bbcfe4eb51bd6f0ba949dfb4ebe148082e
cou <courange.c@gmail.com>
2023-09-28 17:26:03 +0900

    add chocolatey packages

---
+++ cou-memos/tools/chocolateyinstall.ps1
@@ -0,0 +1,15 @@
+﻿$ErrorActionPreference = 'Stop' # stop on all errors
+$toolsDir   = "$(Split-Path -parent $MyInvocation.MyCommand.Definition)"
+$Version    = $env:BuildVersion
+$url64      = "https://github.com/cou723/cou-memos/releases/download/cou-memos-v"+$Version+"/cou-memos_"+$Version+"_x64-setup.exe"
+
+$packageArgs = @{
+  packageName   = $env:ChocolateyPackageName
+  unzipLocation = $toolsDir
+  fileType      = 'exe'
+  url64bit      = $url64
+  checksum64    = ''
+  checksumType64= 'sha256'
+}
+
+Install-ChocolateyPackage @packageArgs
